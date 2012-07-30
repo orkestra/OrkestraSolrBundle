15817052b6ecd2feefe9d28389c396c2e1a15a07
Tyler Sommer <sommertm@gmail.com>
2012-07-30 16:54:56 -0600

    Added LICENSE

--- Mapping/Type/TypeInterface.php
+++ Mapping/Type/TypeInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of OrkestraSolrBundle
+ *
+ * Copyright (c) Tyler Sommer <sommertm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Orkestra\Bundle\SolrBundle\Mapping\Type;
 
 interface TypeInterface
